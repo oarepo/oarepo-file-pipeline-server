16406f5ef306d994040487f84b09d6b0772524ba
senyaaa <pogodars@cvut.cz>
2025-01-22 10:21:01 +0100

    Github workflows, building package

---
+++ run-tests.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+PYTHON="${PYTHON:-python3.12}"
+
+set -e
+
+OAREPO_VERSION="${OAREPO_VERSION:-12}"
+
+if [ "$0" = "$BASH_SOURCE" ]; then
+  trap "clean_up" EXIT
+fi
+
+export VENV=".venv"
+
+setup_test_venv(){
+(
+  echo "Setting up the testing venv"
+
+  if test -d $VENV ; then
+    rm -rf $VENV
+  fi
+
+  $PYTHON -m venv $VENV
+  . $VENV/bin/activate
+  pip install -U setuptools pip wheel
+
+  pip install -e ".[tests]"
+  pip install aiohttp
+  pip install stream-zip
+  pip install pillow
+  pip install pytest-random-order
+  echo "Testing venv is set up"
+)
+}
+
+run_tests(){
+  . $VENV/bin/activate
+  echo "Running tests..."
+  pytest tests --random-order -v
+}
+
+if [ "$0" = "$BASH_SOURCE" ]; then
+    setup_test_venv
+    run_tests
+else
+    echo "This script includes the following functions:"
+    echo "5. setup_test_venv: Sets up a virtual environment for testing."
+    echo "6. run_tests: Runs pytest."
+    echo ""
+    echo "To execute the script, run it directly: ./run-tests.sh"
+fi
---
+++ setup.py
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 CESNET z.s.p.o.
+#
+# oarepo-file-pipeline-server is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+#
+"""Setup module for oarepo_file_pipeline_server."""
+
+from setuptools import setup
+
+setup()
